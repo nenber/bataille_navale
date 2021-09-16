07f7136b17bc8ed756bb5a7893e440c2b067a0d1
nath026 <nathalie.ton026@gmail.com>
2021-09-16 15:09:15 +0200

    adding a hit route

--- bataille.go
+++ bataille.go
@@ -4,70 +4,56 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"time"
+	"strconv"
 )
 
 func main() {
-    http.HandleFunc("/", TimeHandler)
-		http.HandleFunc("/add", AddHandler)
-		http.HandleFunc("/entries", EntriesHandler)
+		http.HandleFunc("/hit", HitHandler)
+		http.HandleFunc("/board", BoardHandler)
+		http.HandleFunc("/boats", BoatsHandler)
     fmt.Println("Server started at port 4567")
     log.Fatal(http.ListenAndServe(":4567", nil))
 }
 
-// GET /
-func TimeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-    fmt.Fprintf(w, "Hello, there\nMethod is allowed\n")
-	} else {
-		currentTime := time.Now()
-    fmt.Fprintf(w, "Il est %dh%d.\n",currentTime.Hour(), currentTime.Local().Minute())
-	}
-}
-
-// POST /add
-func AddHandler(w http.ResponseWriter, r *http.Request) {
+// POST /hit
+func HitHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	if r.Method != "POST" {
     fmt.Fprintf(w, "Hello, there\nMethod not supported")
 	} else {
-    author:= r.Form.Get("author")
-		entry:= r.Form.Get("entry")
-			if (len(author) <=0 || len(entry)<=0) {
-				fmt.Fprintf(w, "Missing parameters")
-			}else{
-				addEntry(entry)
-				fmt.Fprintf(w,author+": "+entry)
+			get_hit_x:= r.Form.Get("hit_x")
+			get_hit_y:=r.Form.Get("hit_y")
+			hit_x := 0
+			hit_y := 0
+			if x, err := strconv.Atoi(get_hit_x); err == nil {
+				fmt.Printf("i=%d,", x)
+				hit_x = x
 			}
+			if y, err := strconv.Atoi(get_hit_y); err == nil {
+				fmt.Printf("i=%d,", y)
+				hit_y = y
+			}
+			if (hit_x > 9 || hit_x < 0 || hit_y > 9 || hit_y < 0) {
+				fmt.Fprintf(w, "Valeur invalide")
+			}
+			// check if it hits the boat on the board
 	}
 }
 
-func addEntry(entry string){
-	fmt.Println(entry)
-	file, err := os.OpenFile("test.txt", os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		log.Fatalf("failed opening file: %s", err)
-	}
-	defer file.Close()
-
-	_, err2 := file.WriteString(entry + "\n")
-	if err2 != nil {
-		log.Fatalf("failed writing to file: %s", err2)
-	}
-}
-
-// GET /entries
-
-func EntriesHandler(w http.ResponseWriter, r *http.Request) {
+// GET /board
+func BoardHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
     fmt.Fprintf(w, "Hello, there\nOnly GET method is allowed")
 	}else{
-		data, err := os.ReadFile("entries.txt")
-	if err != nil {
-		log.Panicf("failed reading data from file: %s", err)
+    fmt.Fprintf(w, "Board")
 	}
-	fmt.Printf("%s", data)
-	fmt.Fprintf(w,"%s", data)
+}
+
+// GET /boats
+func BoatsHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+    fmt.Fprintf(w, "Hello, there\nOnly GET method is allowed")
+	}else{
+    fmt.Fprintf(w, "How many boats")
 	}
 }
