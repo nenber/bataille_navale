d7e132bf108b1f67f93256d6e4c36f3edde2df19
berenger piedagnel <nenberpiedagnel@gmail.com>
2021-09-17 09:22:44 +0000

    set routes as asynchronous

--- bataille.go
+++ bataille.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 	"time"
 )
+
 type battleShipBoard struct {
 	Board [10][10]boardState
 }
@@ -24,13 +25,17 @@ const (
 func random(min int, max int) int {
 	return rand.Intn(max-min) + min
 }
+func runServer() {
+	go http.HandleFunc("/hit", HitHandler)
+	go http.HandleFunc("/board", BoardHandler)
+	go http.HandleFunc("/boats", BoatsHandler)
+	// fmt.Println("Server started at port 4567")
+	log.Fatal(http.ListenAndServe(":4567", nil))
+
+}
 
 func main() {
-	http.HandleFunc("/hit", HitHandler)
-	http.HandleFunc("/board", BoardHandler)
-	http.HandleFunc("/boats", BoatsHandler)
-	fmt.Println("Server started at port 4567")
-	log.Fatal(http.ListenAndServe(":4567", nil))
+	go runServer()
 	rand.Seed(time.Now().UnixNano())
 
 	grid := [10][10]int{{}, {}}
@@ -150,43 +155,43 @@ func placeShip(size int, bo battleShipBoard) battleShipBoard {
 func HitHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	if r.Method != "POST" {
-    fmt.Fprintf(w, "Hello, there\nMethod not supported")
+		fmt.Fprintf(w, "Hello, there\nMethod not supported")
 	} else {
-			get_hit_x:= r.Form.Get("hit_x")
-			get_hit_y:=r.Form.Get("hit_y")
-			hit_x := 0
-			hit_y := 0
-			if x, err := strconv.Atoi(get_hit_x); err == nil {
-				fmt.Printf("i=%d,", x)
-				hit_x = x
-			}
-			if y, err := strconv.Atoi(get_hit_y); err == nil {
-				fmt.Printf("i=%d,", y)
-				hit_y = y
-			}
-			if (hit_x > 9 || hit_x < 0 || hit_y > 9 || hit_y < 0) {
-				fmt.Fprintf(w, "Valeur invalide")
-			}
-			// check if it hits the boat on the board
+		get_hit_x := r.Form.Get("hit_x")
+		get_hit_y := r.Form.Get("hit_y")
+		hit_x := 0
+		hit_y := 0
+		if x, err := strconv.Atoi(get_hit_x); err == nil {
+			fmt.Printf("i=%d,", x)
+			hit_x = x
+		}
+		if y, err := strconv.Atoi(get_hit_y); err == nil {
+			fmt.Printf("i=%d,", y)
+			hit_y = y
+		}
+		if hit_x > 9 || hit_x < 0 || hit_y > 9 || hit_y < 0 {
+			fmt.Fprintf(w, "Valeur invalide")
+		}
+		// check if it hits the boat on the board
 	}
 }
 
 // GET /board
 func BoardHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
-    fmt.Fprintf(w, "Hello, there\nOnly GET method is allowed")
-	}else{
-		board:= [10][10]int{{}, {}}
+		fmt.Fprintf(w, "Hello, there\nOnly GET method is allowed")
+	} else {
+		board := [10][10]int{{}, {}}
 		fmt.Println(board)
-    fmt.Fprintf(w, "Board")
+		fmt.Fprintf(w, "Board")
 	}
 }
 
 // GET /boats
 func BoatsHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
-    fmt.Fprintf(w, "Hello, there\nOnly GET method is allowed")
-	}else{
-    fmt.Fprintf(w, "How many boats")
+		fmt.Fprintf(w, "Hello, there\nOnly GET method is allowed")
+	} else {
+		fmt.Fprintf(w, "How many boats")
 	}
 }
