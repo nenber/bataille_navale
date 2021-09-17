53f8c36a217176fa813583fa601dc44d13cc692a
berenger piedagnel <nenberpiedagnel@gmail.com>
2021-09-17 14:57:29 +0000

    input des ip des adversaires

--- bataille.go
+++ bataille.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"math/rand"
+	"net"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"time"
 )
+
 type battleShipBoard struct {
 	Board [10][10]boardState
 }
@@ -21,25 +27,125 @@ const (
 	stateAttempt boardState = iota // 3
 )
 
+func ParseIPPort(s string) (ip net.IP, port, space string, err error) {
+	ip = net.ParseIP(s)
+	if ip == nil {
+		var host string
+		host, port, err = net.SplitHostPort(s)
+		if err != nil {
+			return
+		}
+		if port != "" {
+			// This check only makes sense if service names are not allowed
+			if _, err = strconv.ParseUint(port, 10, 16); err != nil {
+				return
+			}
+		}
+		ip = net.ParseIP(host)
+	}
+	if ip == nil {
+		err = errors.New("invalid address format")
+	} else {
+		space = "IPv6"
+		if ip4 := ip.To4(); ip4 != nil {
+			space = "IPv4"
+			ip = ip4
+		}
+	}
+	return
+}
+
 func random(min int, max int) int {
 	return rand.Intn(max-min) + min
 }
+func runServer(port int) {
+	go http.HandleFunc("/hit", HitHandler)
+	go http.HandleFunc("/board", BoardHandler)
+	go http.HandleFunc("/boats", BoatsHandler)
+	// fmt.Println("Server started at port 4567")
+	addr := ":" + strconv.Itoa(port)
+	log.Fatal(http.ListenAndServe(addr, nil))
+
+}
 
 func main() {
-	http.HandleFunc("/hit", HitHandler)
-	http.HandleFunc("/board", BoardHandler)
-	http.HandleFunc("/boats", BoatsHandler)
-	fmt.Println("Server started at port 4567")
-	log.Fatal(http.ListenAndServe(":4567", nil))
+	myPort := flag.String("port", ":4567", "Port of server")
+
+	// opponent := flag.String("opp", "default value", "Opponent")
+	flag.Parse()
+
+	// ip, port, space, err := ParseIPPort(*opponent)
+
+	// fmt.Println(ip, ":", port, "   ", space)
+	number, err := strconv.ParseInt(*myPort, 10, 32)
+	if err != nil {
+		fmt.Println(err)
+	}
+	finalIntNum := int(number)
+	println(finalIntNum)
+
+	go runServer(finalIntNum)
 	rand.Seed(time.Now().UnixNano())
 
 	grid := [10][10]int{{}, {}}
 	fmt.Println(grid[0])
 	board := makeBoard()
-	for i := 0; i < 9; i++ {
+	for i := 0; i < 10; i++ {
 		fmt.Println(board.Board[i])
 	}
 
+	reader := bufio.NewReader(os.Stdin)
+	askOpponentTarget(reader)
+
+}
+
+func askOpponentTarget(reader *bufio.Reader) {
+	var opponentsAdrr []string
+
+	for i := 0; ; i++ {
+		if i == 0 {
+			println("Adresse de l'adversaire #", i+1, " (sous la forme \"127.0.0.1:4567\") : ")
+			scanner := bufio.NewScanner(os.Stdin)
+			scanner.Scan()
+			result := scanner.Text()
+
+			ip, port, space, err := ParseIPPort(result)
+			_ = space
+			if err != nil {
+				println("L'adresse n'est pas bonne")
+				i--
+			} else {
+				addr := ip.String() + ":" + port
+				opponentsAdrr = append(opponentsAdrr, addr)
+
+			}
+
+		} else {
+			println("Adresse de l'adversaire #", i+1, " (sous la forme \"127.0.0.1:4567\") (jouer(j)) : ")
+			scanner := bufio.NewScanner(os.Stdin)
+			scanner.Scan()
+			result := scanner.Text()
+			if result == "j" || result == "J" {
+				break
+			}
+
+			ip, port, space, err := ParseIPPort(result)
+			_ = space
+			if err != nil {
+				println("L'adresse n'est pas bonne")
+				i--
+			} else {
+				addr := ip.String() + ":" + port
+				opponentsAdrr = append(opponentsAdrr, addr)
+
+			}
+		}
+
+	}
+	println("Voici les adresses de tes adversaires : ")
+	for i := 0; i < len(opponentsAdrr); i++ {
+		println("- ", opponentsAdrr[i])
+	}
 }
 
 func (b *battleShipBoard) Draw() string {
@@ -150,43 +256,43 @@ func placeShip(size int, bo battleShipBoard) battleShipBoard {
 func HitHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	if r.Method != "POST" {
-    fmt.Fprintf(w, "Hello, there\nMethod not supported")
+		fmt.Fprintf(w, "Hello, there\nMethod not supported")
 	} else {
-			get_hit_x:= r.Form.Get("hit_x")
-			get_hit_y:=r.Form.Get("hit_y")
-			hit_x := 0
-			hit_y := 0
-			if x, err := strconv.Atoi(get_hit_x); err == nil {
-				fmt.Printf("i=%d,", x)
-				hit_x = x
-			}
-			if y, err := strconv.Atoi(get_hit_y); err == nil {
-				fmt.Printf("i=%d,", y)
-				hit_y = y
-			}
-			if (hit_x > 9 || hit_x < 0 || hit_y > 9 || hit_y < 0) {
-				fmt.Fprintf(w, "Valeur invalide")
-			}
-			// check if it hits the boat on the board
+		get_hit_x := r.Form.Get("hit_x")
+		get_hit_y := r.Form.Get("hit_y")
+		hit_x := 0
+		hit_y := 0
+		if x, err := strconv.Atoi(get_hit_x); err == nil {
+			fmt.Printf("i=%d,", x)
+			hit_x = x
+		}
+		if y, err := strconv.Atoi(get_hit_y); err == nil {
+			fmt.Printf("i=%d,", y)
+			hit_y = y
+		}
+		if hit_x > 9 || hit_x < 0 || hit_y > 9 || hit_y < 0 {
+			fmt.Fprintf(w, "Valeur invalide")
+		}
+		// check if it hits the boat on the board
 	}
 }
 
 // GET /board
 func BoardHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
-    fmt.Fprintf(w, "Hello, there\nOnly GET method is allowed")
-	}else{
-		board:= [10][10]int{{}, {}}
+		fmt.Fprintf(w, "Hello, there\nOnly GET method is allowed")
+	} else {
+		board := [10][10]int{{}, {}}
 		fmt.Println(board)
-    fmt.Fprintf(w, "Board")
+		fmt.Fprintf(w, "Board")
 	}
 }
 
 // GET /boats
 func BoatsHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
-    fmt.Fprintf(w, "Hello, there\nOnly GET method is allowed")
-	}else{
-    fmt.Fprintf(w, "How many boats")
+		fmt.Fprintf(w, "Hello, there\nOnly GET method is allowed")
+	} else {
+		fmt.Fprintf(w, "How many boats")
 	}
 }
